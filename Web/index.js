a5d9cddd18c913e68f40c32d2aadea91d8d436a7
Ken-Jet <rachelmaniac535@gmail.com>
2025-12-23 14:07:28 +0700

    update kodingan

--- Web/index.js
+++ Web/index.js
@@ -1,160 +1,222 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(SplitText);
 
-var tl1 =gsap.timeline({
-    onComplete : () => {
-        gsap.set('.box-1', {
-            position : 'sticky',
-            top : 0,
-            zIndex : 1000
+if(!sessionStorage.getItem('anim1')){
+    var tl1 =gsap.timeline({
+        onComplete : () => {
+            // gsap.set('.box-1', {
+            //     position : 'sticky',
+            //     top : 0,
+            //     zIndex : 1000
+            // })
+
+            sessionStorage.setItem('anim1', 'true');
+        }
+    });
+
+    document.querySelector('.bulat-1').addEventListener('mouseenter', () => {
+        gsap.to('.bulat-1',{
+            duration : 1,
+            opacity : 1,
+            ease : 'power4.out'
         })
-    }
-});
-
-document.querySelector('.bulat-1').addEventListener('mouseenter', () => {
-    gsap.to('.bulat-1',{
-        duration : 1,
-        opacity : 1,
-        ease : 'power4.out'
     })
-})
 
-document.querySelector('.bulat-1').addEventListener('mouseleave', () => {
-    gsap.to('.bulat-1',{
+    document.querySelector('.bulat-1').addEventListener('mouseleave', () => {
+        gsap.to('.bulat-1',{
+            duration : 1,
+            opacity : 0,
+            ease : 'power4.out'
+        })
+    })
+
+    tl1.from('.box-1',{
+        duration : 1.25,
+        y : -100,
+        ease : 'expo.out'
+    })
+
+    .from('.box-2',{
+        duration: 1,
+        opacity : 0,
+        y: 100,
+        ease : 'power4.out'
+    }, '-=1')
+
+    .from('.box-3',{
         duration : 1,
         opacity : 0,
         ease : 'power4.out'
-    })
-})
+    },'-=0.75')
 
-tl1.from('.box-1',{
-    duration : 1.25,
-    y : -100,
-    ease : 'expo.out'
-})
+} else {
+    // gsap.set('.box-1', {
+    //             position : 'sticky',
+    //             top : 0,
+    //             zIndex : 1000
+    // })
 
-.from('.box-2',{
-    duration: 1,
-    opacity : 0,
-    y: 100,
-    ease : 'power4.out'
-}, '-=1')
-
-.from('.box-3',{
-    duration : 1,
-    opacity : 0,
-    ease : 'power4.out'
-},'-=0.75')
-
-var tl2 = gsap.timeline({
-    scrollTrigger: {
-        trigger : '.box-4',
-    }
-})
-
-tl2.from('.box-4',{
-    duration : 1,
-    opacity : 0,
-    y : 200,
-    ease : 'power4.out'
-})
-
-.from('.box-5', {
-    duration : 1,
-    x : -1000,
-    ease : 'power4.out'
-},0.3)
-
-.from('.box-6', {
-    duration : 1,
-    x : 1000,
-    ease : 'power4.out'
-},0.3)
-
-
-var tl3 = gsap.timeline({
-    scrollTrigger : {
-        trigger : '.teks-1',
-        markers: false,
-        start : '-200px bottom '
-    }
-})
-
-tl3.from('.teks-1', {
-    duration : 1,
-    opacity : 0,
-    y : 500,
-    ease : 'power4.out'
-})
-
-.from('.box-7', {
-    duration : 1,
-    opacity : 0,
-    y : 500,
-    ease : 'back.out'
-}, 0.2)
-
-.from('.box-8', {
-    duration : 1,
-    opacity : 0,
-    y : 500,
-    ease : 'back.out'
-}, 0.4)
-
-.from('.box-9', {
-    duration : 1,
-    opacity : 0,
-    y : 500,
-    ease : 'back.out'
-}, 0.6)
-
-.from('.box-10', {
-    duration : 1,
-    opacity : 0,
-    y : 500,
-    ease : 'back.out'
-}, 0.8)
-
-function hoverBox (param1) {
-    document.querySelector(param1).addEventListener('mouseover', () => {
-        gsap.to(param1, {
-            y : -40,
-            duration : 0.5,
-            ease : 'power2.out'
+    document.querySelector('.bulat-1').addEventListener('mouseenter', () => {
+        gsap.to('.bulat-1',{
+            duration : 1,
+            opacity : 1,
+            ease : 'power4.out'
         })
     })
-    document.querySelector(param1).addEventListener('mouseout', () => {
-        gsap.to(param1, {
-            y : 0,
-            duration : 0.5
+
+    document.querySelector('.bulat-1').addEventListener('mouseleave', () => {
+        gsap.to('.bulat-1',{
+            duration : 1,
+            opacity : 0,
+            ease : 'power4.out'
         })
     })
 }
 
-hoverBox('.box-7');
-hoverBox('.box-8');
-hoverBox('.box-9');
-hoverBox('.box-10');
+if(!sessionStorage.getItem('anim2')){
+    var tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger : '.box-4',
+        },
+        onComplete : () => {
+            sessionStorage.setItem('anim2', 'true');
+        }
+    })
 
-var tl4 = gsap.timeline({
-    scrollTrigger : {
-        trigger : '.box-11'
+    tl2.from('.box-4',{
+        duration : 1,
+        opacity : 0,
+        y : 200,
+        ease : 'power4.out'
+    })
+
+    .from('.box-5', {
+        duration : 1,
+        x : -1000,
+        ease : 'power4.out'
+    },0.3)
+
+    .from('.box-6', {
+        duration : 1,
+        x : 1000,
+        ease : 'power4.out'
+    },0.3)
+}
+
+if(!sessionStorage.getItem('anim3')){
+    var tl3 = gsap.timeline({
+        scrollTrigger : {
+            trigger : '.teks-1',
+            markers: false,
+            start : '-200px bottom '
+        },
+        onComplete : () => {
+            sessionStorage.setItem('anim3', 'true')
+        }
+    })
+
+    tl3.from('.teks-1', {
+        duration : 1,
+        opacity : 0,
+        y : 500,
+        ease : 'power4.out'
+    })
+
+    .from('.box-7', {
+        duration : 1,
+        opacity : 0,
+        y : 500,
+        ease : 'back.out'
+    }, 0.2)
+
+    .from('.box-8', {
+        duration : 1,
+        opacity : 0,
+        y : 500,
+        ease : 'back.out'
+    }, 0.4)
+
+    .from('.box-9', {
+        duration : 1,
+        opacity : 0,
+        y : 500,
+        ease : 'back.out'
+    }, 0.6)
+
+    .from('.box-10', {
+        duration : 1,
+        opacity : 0,
+        y : 500,
+        ease : 'back.out'
+    }, 0.8)
+
+    function hoverBox (param1) {
+        document.querySelector(param1).addEventListener('mouseover', () => {
+            gsap.to(param1, {
+                y : -40,
+                duration : 0.5,
+                ease : 'power2.out'
+            })
+        })
+        document.querySelector(param1).addEventListener('mouseout', () => {
+            gsap.to(param1, {
+                y : 0,
+                duration : 0.5
+            })
+        })
     }
-})
 
-tl4.from('.box-11', {
-    opacity : 0,
-    y : 200,
-    duration : 1,
-    ease : 'power4.out'
-})
+    hoverBox('.box-7');
+    hoverBox('.box-8');
+    hoverBox('.box-9');
+    hoverBox('.box-10');
+} else {
+    function hoverBox (param1) {
+        document.querySelector(param1).addEventListener('mouseover', () => {
+            gsap.to(param1, {
+                y : -40,
+                duration : 0.5,
+                ease : 'power2.out'
+            })
+        })
+        document.querySelector(param1).addEventListener('mouseout', () => {
+            gsap.to(param1, {
+                y : 0,
+                duration : 0.5
+            })
+        })
+    }
 
-.from('.box-12',{
-    opacity : 0,
-    y : 300,
-    duration : 1,
-    ease : 'power1.out'
-}, 0.3)
+    hoverBox('.box-7');
+    hoverBox('.box-8');
+    hoverBox('.box-9');
+    hoverBox('.box-10');
+}
+
+if(!sessionStorage.getItem('anim4')){
+    var tl4 = gsap.timeline({
+        scrollTrigger : {
+            trigger : '.box-11'
+        },
+        onComplete : () => {
+            sessionStorage.setItem('anim4' , 'true');
+        }
+    })
+
+    tl4.from('.box-11', {
+        opacity : 0,
+        y : 200,
+        duration : 1,
+        ease : 'power4.out'
+    })
+
+    .from('.box-12',{
+        opacity : 0,
+        y : 300,
+        duration : 1,
+        ease : 'power1.out'
+    }, 0.3)
+}
 
 const swiper1 = new Swiper ('.swiper1', {
     slidesPerView : 5,
@@ -236,38 +298,124 @@ tl5.to('.tim-3', {
     delay : 0.75
 })
 
-var tl6 = gsap.timeline({
-    scrollTrigger : {
-        trigger : '.box-13',
-        markers : false,
-        start : '-250px bottom'
-    }
-})
+if(!sessionStorage.getItem('anim5')){
+    var tl6 = gsap.timeline({
+        scrollTrigger : {
+            trigger : '.box-13',
+            markers : false,
+            start : '-250px bottom'
+        },
+        onComplete : () => {
+            sessionStorage.setItem('anim5', 'true');
+        }
+    })
 
-var split1 = SplitText.create('.kata-1', {type : 'words , chars'});
+    var split1 = SplitText.create('.kata-1', {type : 'words , chars'});
 
-tl6.from('.box-13', {
-    width : 0,
-    height : 0,
-    duration : 1,
-    ease : 'power4.out',
-    y : 500,
-    opacity : 0
-})
+    tl6.from('.box-13', {
+        width : 0,
+        height : 0,
+        duration : 1,
+        ease : 'power4.out',
+        y : 500,
+        opacity : 0
+    })
 
-.from (split1.chars, {
-    duration : 0.75,
-    opacity : 0,
-    y : 200,
-    ease : 'power3.out',
-    stagger : 0.04
-}, 1)
+    .from (split1.chars, {
+        duration : 0.75,
+        opacity : 0,
+        y : 200,
+        ease : 'power3.out',
+        stagger : 0.04
+    }, 1)
 
-.from('.box-14', {
-    duration : 0.75,
-    opacity : 0,
-    y : 200,
-    ease : 'power3.out'
-}, '-=0.5')
+    .from('.box-14', {
+        duration : 0.75,
+        opacity : 0,
+        y : 200,
+        ease : 'power3.out'
+    }, '-=0.5')
+}
 
 //GSDevTools.create({animation : tl6});
+
+//Ux animation
+
+document.addEventListener('alpine:init', () => {
+    Alpine.store('dropdown', {
+        active: null
+    })
+})
+
+function bar1 (id) {
+    return {
+        id,
+        open : false ,
+        tl : null,
+        init () {
+            this.$nextTick (() => {
+                this.tl = gsap.timeline({paused : true})
+
+                .to(this.$refs.bar1 ,{
+                    height : 'auto',
+                    duration : 0.25,
+                    padding : '10px'
+                })
+
+                .fromTo(this.$refs.bar1.children, {
+                    x : 300,
+                    opacity : 0
+                },{
+                    x : 0,
+                    duration : 0.5,
+                    opacity : 1,
+                    ease : 'power4.out',
+                    stagger : 0.2,
+                })
+            })
+
+            this.$watch(
+                () => Alpine.store('dropdown').active,
+                value => {
+                    if (value !== this.id && this.open) {
+                        this.forceClose()
+                    }
+                }
+            )
+
+            this.$watch('open' , value => {
+                value ? this.tl.play() : this.tl.reverse()
+            })
+        },
+        toggle () {
+             const store = Alpine.store('dropdown')
+
+            if (this.open) {
+                this.close()
+                store.active = null
+            } else {
+                this.open = true
+                store.active = this.id
+            }
+        },
+        close () {
+            this.open = false
+        },
+        forceClose() {
+            this.tl.pause(0)
+
+            gsap.set(this.$refs.menu, {
+                height: 0,
+                padding: 0
+            })
+
+            gsap.set(this.$refs.menu.children, {
+                opacity: 0,
+                x: 300,
+                scale: 0
+            })
+
+            this.open = false
+        }
+    }
+}
